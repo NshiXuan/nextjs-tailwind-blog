53345249656ba565e9f2ee00328fa75e7e3579f3
nsx <1743091664@qq.com>
2023-03-16 18:38:39 +0800

    更新header

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: '首页' },
   { href: '/blog', title: '文章' },
-  { href: '/about', title: '关于' },
+  { href: '/about', title: '关于我' },
 ]
 
 export default headerNavLinks
