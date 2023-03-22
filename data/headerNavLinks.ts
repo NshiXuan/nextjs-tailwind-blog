84d9e5352b8108d0b9690782c131d586b0d2c282
nsx <1743091664@qq.com>
2023-03-22 11:09:14 +0800

    添加作品模块

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: '首页' },
   { href: '/blog', title: '文章' },
+  { href: '/projects', title: '作品' },
   { href: '/about', title: '关于我' },
 ]
 
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,9 +1,27 @@
 const projectsData = [
   {
-    title: 'Code-Editor',
-    description: `一个纯前端的在线代码实时预览工具,支持 Less Scss JavaScript Typescript`,
-    imgSrc: '/static/images/code-editor.png',
-    href: 'https://code.runjs.cool/',
+    title: 'airbnb-next',
+    description: `一个使用React、TS、Next、Tailwind CSS仿的Airbnb官网`,
+    imgSrc: '',
+    href: 'https://airbnb-next.netlify.app/',
+  },
+  {
+    title: 'electron',
+    description: `一个使用Electron、Vue3、TS、Tailwind CSS开发的桌面摄像头软件`,
+    imgSrc: '',
+    href: 'https://github.com/NshiXuan/camera-electron',
+  },
+  {
+    title: 'oppo-nuxt',
+    description: `一个使用Vue3、TS、Nuxt3开发的仿OPPO官网`,
+    imgSrc: '',
+    href: 'https://sx-oppo-nuxt.netlify.app/',
+  },
+  {
+    title: 'food-dlivery',
+    description: `一个使用CSS、JS开发的移动端适配的网页`,
+    imgSrc: '',
+    href: 'https://sx-food-dlivery.netlify.app/',
   },
 ]
 
